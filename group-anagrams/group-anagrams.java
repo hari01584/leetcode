a8afce2826a6bb68e2d064d8f66297e8e2f7d06a
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-09-23 15:46:26 +0530

    Time: 6 ms (85.33%), Space: 42.1 MB (79.09%) - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,23 +1,14 @@
 /*
-explanation: the program works by sorting each string alphabetically then adding them to a list of Strings, this list is sorted again and the resulting list is used as base to get anagrams groups! Additionally a class Element is made specifically to store and save the index of initial unsorted string in original array (String[] strs).
+explanation: the program works by sorting each string alphabetically then adding them to a hashmap of list of strings, each of sorted element is added as key with its value being the unsorted item. In the last we loop over all the values and get our required lists of strings!
 
 testcases:
 [""] -> Works
 ["eat","tea","tan","ate","nat","bat"] -> Works
 */
 
-public class Element {
-    public int id;
-    public String data;
-    
-    Element(int id, String data){
-        this.id = id;
-        this.data = data;
-    }
-}
-
-
 class Solution {
+    HashMap<String, ArrayList<String>> hashy = new HashMap<String, ArrayList<String>>();
+
     public String stringSort(String input){
         char[] charArray = input.toCharArray();
         Arrays.sort(charArray);
@@ -28,34 +19,19 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ret = new ArrayList<List<String>>();
         
-        List<Element> stSort = new ArrayList<Element>();
         for(int i=0; i<strs.length; i++){
             String s = stringSort(strs[i]);
-            stSort.add(new Element(i, s));
+            if (hashy.get(s) == null) {
+                hashy.put(s, new ArrayList<String>());
+            }
+            
+            hashy.get(s).add(strs[i]);
+        }
+    
+        for (List<String> value : hashy.values()) {
+            ret.add(value);
         }
         
-        Collections.sort(stSort, new Comparator<Element>() {
-            @Override
-            public int compare(final Element object1, final Element object2) {
-                return object1.data.compareTo(object2.data);
-            }
-        });
-
-        int p = 0;
-        String last = null;
-        List<String> l = null;
-        for(Element e:stSort){
-            System.out.println(e.data);
-            if(!e.data.equals(last)){
-                if(l!=null) ret.add(l);
-                l = new ArrayList<String>();
-                last = e.data;
-            }
-            l.add(strs[e.id]);
-        }
-        //temp.clear();
-        ret.add(l);
-        //ret.add(stSort);
         return ret;
     }
 }
