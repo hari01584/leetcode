d9edc153632a796c987481dab8ed16c6f3ea2ba0
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-01-26 14:03:25 +0530

    Time: 3 ms (37.43%), Space: 38.3 MB (13.51%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,3 +1,13 @@
+/*
+explanation: simple root finder using natural log and exponent power functions! Uses the fact that taking log on both side of equations y^2 = x can give y as e^(log(x)/2), calculating this and performing calibration for boundary line numbers will give the square root in integer.
+
+testcase:
+4 -> Works
+8 -> Works
+
+Time & Space Complexity: O(1) and O(1), Since it only uses mathematical functions exponent and logarithm.
+*/
+
 class Solution {
     public int mySqrt(int x) {
         double r = Math.log(x) * 0.5;
