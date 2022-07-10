81c4aa0eeaf8a0da6146f425e9efd46abc380e41
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-07-11 01:11:51 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -47,75 +47,31 @@ class gfg
 
 class Solution 
 { 
-    static int[] wt;
-    static int[] val;
-    static int limit = 10;
-    
-    static HashMap<String, Integer> map = new HashMap<>();
-    static int[][] dp;
-    
-    static String createbitstr(String str, int index, boolean isInclude){
-        return str.substring(0, index) + (isInclude?"1":"0")
-              + str.substring(index + 1);
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int getElement(int[][] dp, int i, int j){
+        if(i < 0 || j<0) return 0;
+        return dp[i][j];
     }
-    static int knap(int W, int n, String identity){
-        if(W <= 0 || n >= wt.length){
-            return 0;
-        }
-        
-        // System.out.println("getmas "+identity);
-        // if(map.get(identity) != null){
-        //     System.out.println("Saved you some slack! " + wt[n]);
-        //     return map.get(identity);
-        // }
-        if(dp[n][W] != -1){
-            // System.out.println("Saved you some slack! " + wt[n]);
-            return dp[n][W];
-        }
-        
-        // System.out.println("n "+n+" "+"Judgement "+wt[n]+" weight "+W);
-        
-        if(wt[n] > W){
-            int v = knap(W, n+1, identity);
-            // map.put(identity, v);
-            return v;
-        }
-        else{
-            int include = val[n] + knap(W-wt[n], n+1, createbitstr(identity, n, true));
-            int exclude = knap(W, n+1, identity);
-
-            // System.out.println("include "+include+", exclude "+exclude);
-
-            int v = -1;
-            if(include > exclude){
-                v = include;
-                // String mas = createbitstr(identity, n, true);
-                // map.put(mas, v);
-                // System.out.println("savemask "+mas);
-            }
-            else{
-                v = exclude;
-                // map.put(identity, v);
-            }
-            // Cache (ie memoize)
-            
-            dp[n][W] = v;
-            
-            return v;
-        }
-    }
-
-    static int knapSack(int W, int w[], int v[], int n) 
+    static int knapSack(int W, int wt[], int val[], int n) 
     { 
-         wt = w;
-         val = v;
-         dp = new int[n+1][W+1];
-         for (int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i], -1);
-         }
+        int[][] dp = new int[n+1][W+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=W; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(j < wt[i-1]){
+                    dp[i][j] = getElement(dp, i-1, j);
+                    continue;
+                }
+                dp[i][j] = Math.max(getElement(dp, i-1, j), val[i-1] + getElement(dp, i-1, j-wt[i-1]));
+            }
+        }
+        // System.out.println(Arrays.deepToString(dp).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
 
-         String s = String.format("%0" + n + "d", 0);
-        //  System.out.println(s);
-         return knap(W, 0, s);
+         return dp[n][W];
     } 
 }
+
+
