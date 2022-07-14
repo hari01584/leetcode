545cbb6dafe8346a4652729ebfc1f6be68c43c94
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-07-14 20:44:01 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -28,6 +28,7 @@ class GFG
 /*
 explanation: https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
 
+tabulation written by using patterns!
 */
 
 class Solution{
@@ -48,14 +49,47 @@ class Solution{
         return dp[i][j]=min;
     }
     
-    int matrixMultiplication(int N, int arr[])
-    {
-        this.arr = arr;
-        dp = new int[arr.length][arr.length];
-        for(int[] d:dp){
-            Arrays.fill(d, -1);
+    int matrixMultitabular(int N, int arr[]){
+        int[][] dp = new int[N-1][N-1];
+        
+
+        for(int diag=0; diag<N-1; diag++){
+            for(int i=0; i<N-1-diag; i++){
+                int j = i+diag;
+                // System.out.println(i+" "+j+" E");
+                
+                if(i==j) dp[i][j] = 0;
+                else{
+                    int min = Integer.MAX_VALUE;
+                    for(int k=i; k<j; k++){
+                        // System.out.println(i+" "+k+", "+(k+1)+" "+j);
+                        // System.out.println(arr[i]+" "+arr[k]+", "+arr[j]);
+
+                        int mc1 = dp[i][k];
+                        int mc2 = dp[k+1][j];
+                        int mc = mc1 + mc2 + (arr[i] * arr[k+1] * arr[j+1]);
+                        min = Math.min(min, mc);
+                    }
+                    dp[i][j] = min;
+                }
+            }
+            // System.out.println();
         }
         
-        return mcm(1, arr.length-1);
+        // System.out.println(Arrays.deepToString(dp));
+        
+        return dp[0][N-2];
+    }
+    
+    int matrixMultiplication(int N, int arr[])
+    {
+        // this.arr = arr;
+        // dp = new int[arr.length][arr.length];
+        // for(int[] d:dp){
+        //     Arrays.fill(d, -1);
+        // }
+        
+        // return mcm(1, arr.length-1);
+        return matrixMultitabular(N, arr);
     }
 }
