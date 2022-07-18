112ee720f2127bf675c96b4cb27393763f0031c9
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-07-18 13:13:28 +0530

    Time: 8 ms (63.56%), Space: 45.3 MB (62.45%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,63 +1,26 @@
 /*
-explanation: converts roman number to integer one using one stack and in O(n) time!, while scanning the roman string it scans it letter by letter and appends into a stack (of integers), the stack checks the value of topmost element, if this element is less than current to be added ie to be add is V and topmost is I, then it pops the V and substract the new value! After making this stack we add all element together and finally get answer.
-
-testcase:
-MCMXCIV => Works
-Scanning from left to right in order M, C, M, X, C, I, V. for first character M, it is added to stack(note 1000 corresponding M is added, not letter M) (as it is empty), subsequent inserts check the value of previous inserts and add normally in stack (if top is not less than current element) or substracts the value!
+explanation: https://leetcode.com/problems/roman-to-integer/discuss/6529/My-solution-for-this-question-but-I-don't-know-is-there-any-easier-way
 */
-
-class RomanStack {
-    Stack<Integer> _stack;
-    char top = 'a';
-    char sub = 'a';
-    
-    RomanStack(){
-        _stack = new Stack<Integer>();
-    }
-    int fenc(char c){
-        if(c=='I') return 1;
-        else if(c=='V') return 5;
-        else if(c=='X') return 10;
-        else if(c=='L') return 50;
-        else if(c=='C') return 100;
-        else if(c=='D') return 500;
-        else if(c=='M') return 1000;
-        return -1;
-    }
-    void push(char c){
-        if(_stack.empty()){
-            top = c;
-            _stack.push(fenc(c));
-            return;
-        }
-        int i = fenc(c);
-        int top = _stack.peek();
-        if(top < i){
-            // Lesser than
-            top = _stack.pop();
-            top = i - top;
-            _stack.push(top);
-        }
-        else{
-            _stack.push(i);
-        }
-    }
-    
-    int addAll(){
-        int s = 0;
-        while(!_stack.isEmpty()){
-            s += _stack.pop();
-        }
-        return s;
-    }
-}
-
 class Solution {
     public int romanToInt(String s) {
-        RomanStack stack = new RomanStack();
+        int sum=0;
+        if(s.indexOf("IV")!=-1){sum-=2;}
+        if(s.indexOf("IX")!=-1){sum-=2;}
+        if(s.indexOf("XL")!=-1){sum-=20;}
+        if(s.indexOf("XC")!=-1){sum-=20;}
+        if(s.indexOf("CD")!=-1){sum-=200;}
+        if(s.indexOf("CM")!=-1){sum-=200;}
+
         for(int i=0; i<s.length(); i++){
-            stack.push(s.charAt(i));
+            if(s.charAt(i) == 'I') sum+=1;
+            else if(s.charAt(i) == 'V') sum+=5;
+            else if(s.charAt(i) == 'X') sum+=10;
+            else if(s.charAt(i) == 'L') sum+=50;
+            else if(s.charAt(i) == 'C') sum+=100;
+            else if(s.charAt(i) == 'D') sum+=500;
+            else if(s.charAt(i) == 'M') sum+=1000;
         }
-        return stack.addAll();
+        
+        return sum;
     }
 }
