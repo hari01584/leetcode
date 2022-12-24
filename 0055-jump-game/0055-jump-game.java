0cc3973db925534ec5b8fcb508dc32e577314efc
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-12-24 14:13:24 +0530

    Time: 496 ms (12.77%), Space: 43.4 MB (74.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,25 @@
+/*
+explanation: jump game using tabulation dp! The program works by defining a reachability array (reach) where reach[i] the ability of that element to be reached, if this is true => it can be reached by 0th element, else it cannot! We do this for step by step and find the state of reachability by last element, if this is true then it means we can reach by jumping, else not!
+
+testcase: [2,3,1,1,4] -> Works
+
+Time & Space Complexity: O(n^2) & O(n): Time complexity is quadratic because each element in worst case might need to traverse to end of array to set reachability true, also space complexity is linear due to storage required by reach array!
+*/
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean[] reach = new boolean[nums.length]; // Reachability array!
+        reach[0] = true; // First element is automatically reachable!
+        
+        for(int i=0; i<reach.length; i++){
+            // If this (current) element is reachable then set reachability of all elements possible to reach by this to be true!
+            if(reach[i]){
+                for(int j=i+1; j<=Math.min(nums.length-1, i+nums[i]); j++){
+                    reach[j] = true;
+                }
+            }
+        }
+        
+        // Finally return reachability of last element, if this is true => can be reached, else not!
+        return reach[reach.length-1];
+    }
+}
