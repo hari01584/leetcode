ba9f6f42ea178560de8c49c98f0e5fb7257ba4f7
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2023-01-05 18:50:52 +0530

    Time: 3 ms (44.94%), Space: 44.4 MB (14.33%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,28 +1,30 @@
 /*
-explanation: generate parenthesis by using the generator function and recursively applying string operations on them!
+explanation: generate parentheses using dynamic programming! The algorithm works by crawling the string and using conditions to add both parenthesis! which is to always add left parenthesis if its leftcount (lp) is > 0, and only add right parenthesis if rightcount is greater than left parenthesis!
 
 testcase: 3 -> Works
 
-Time & Space Complexity: O(n^2) & O(n): Time complexity is quadratic since each loop base function iterates 2 times deeper into program, space complexity is linear since stack space required!
+Time & Space Complexity: O(2^n) & O(n): Since each recursive calls 2 other function therefore max tc is 2 raised to power n, also space complexity is linear due to stack space required in algorithm!
 */
-class Solution {    
-    public HashSet<String> _generateParenthesis(int n) {
-        if(n==1){
-            return new HashSet<String>(Arrays.asList("()"));
+class Solution {
+    List<String> ans = new ArrayList<>();
+    
+    void backparen(String str, int lp, int rp){
+        if(rp < lp) return;
+        if(lp == rp && rp == 0){
+            // Return and add this element
+            ans.add(str);
+            return;
         }
-        HashSet<String> res = new HashSet<String>();
-        HashSet<String> next = _generateParenthesis(n-1);
-        for(String st : next){
-            for(int i=0; i<st.length(); i++){
-                String newst = st.substring(0, i) + "()" + st.substring(i, st.length());
-                res.add(newst);
-            }
-        }
-        return res;
+        if(lp < 0 || rp < 0) return;
+        
+        // Add left paren to str
+        backparen(str+"(", lp-1, rp);
+        // Add right paren to str
+        backparen(str+")", lp, rp-1);
     }
     
     public List<String> generateParenthesis(int n) {
-        HashSet<String> ans = _generateParenthesis(n);
-        return new ArrayList<>(ans);
+        backparen("", n, n);
+        return ans;
     }
 }
