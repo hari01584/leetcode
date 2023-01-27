d52174d3837b1c9e3650e8deec3967e5c9017fda
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2023-01-27 16:52:23 +0530

    Time: 38 ms (7.86%), Space: 46.6 MB (51.89%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,33 +1,89 @@
-/*
-*/
 class Solution {
+    int[] boardmap;
+    int[] dp;
+    
+//     public int dysnakestep(int n){
+//         if(n == boardmap.length-1) return 0;
+//         if(n > boardmap.length-1) return 100000; // Overbound
+//         if(dp[n] != -1) return dp[n];
+        
+//         // System.out.println("Calling for "+n);
+
+//         // Start at n and find paths!
+//         int min = 100000;
+//         for(int i=n+1; i<=n+6; i++){
+//             int target = i;
+//             if(i >= boardmap.length) break;
+//             if(boardmap[target] != -1) target = boardmap[target];
+//             // System.out.println("Checking for "+i+" bmt:"+boardmap[i]);
+//             if(target < i) continue; // Skip snakes
+//             int ss = 1 + dysnakestep(target);
+            
+//             min = Math.min(min, ss);
+//         }
+        
+//         // System.out.println("At "+n+" return minsteps: "+min);
+//         return dp[n]=min;
+//     }
+    
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Queue<Integer> queue = new LinkedList<>();
+        boardmap = new int[n*n + 1];
+        dp = new int[boardmap.length+1];
+        Arrays.fill(dp, -1);
+        
+        boolean linear = n % 2 == 0;
+        
+        int c = n*n;
+        for(int[] b : board){
+            if(linear){
+                for(int i=0; i<b.length; i++){
+                    boardmap[c--] = b[i];
+                }
+            } else {
+                for(int i=b.length-1; i>=0; i--){
+                    boardmap[c--] = b[i];
+                }
+            }
+            linear = !linear;
+        }
+        
+        // for(int i=1; i<= n*n; i++){
+        //     if(boardmap[i] == -1) continue;
+        //     System.out.println(i+" maps to "+boardmap[i]);
+        // }
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int[] reachcost = new int[boardmap.length];
+        Arrays.fill(reachcost, Integer.MAX_VALUE);
+        reachcost[1] = 0;
         queue.offer(1);
-        boolean[] visited = new boolean[n * n + 1];
-        for (int move = 0; !queue.isEmpty(); move++) {
-            for (int size = queue.size(); size > 0; size--) {
-                int num = queue.poll();
-                if (visited[num]) continue;
-                visited[num] = true;
-                if (num == n * n) return move;
-                for (int i = 1; i <= 6 && num + i <= n * n; i++) {
-                    int next = num + i;
-                    int value = getBoardValue(board, next);
-                    if (value > 0) next = value;
-                    if (!visited[next]) queue.offer(next);
+        
+        while(queue.size() > 0){
+            int p = queue.poll();
+            // Check for reachable nodes and set values!
+            int curcost = reachcost[p];
+            // System.out.println("Processing "+p+" with current reach: "+curcost);
+
+            for(int i=p+1; i<=p+6; i++){
+                if(i>n*n) break;
+                int ncost = curcost + 1;
+                // Find target node
+                int target = i;
+                if(boardmap[i] != -1) target = boardmap[i];
+                
+                // Check if this cost better
+                if(ncost < reachcost[target]){
+                    // Yes. add it to queue!
+                    reachcost[target] = ncost;
+                    queue.add(target);
                 }
             }
         }
-        return -1;
+        // return ans;
+        
+        int ans = reachcost[n*n];
+        if(ans == Integer.MAX_VALUE) return -1;
+        return ans;
     }
-
-    private int getBoardValue(int[][] board, int num) {
-        int n = board.length;
-        int r = (num - 1) / n;
-        int x = n - 1 - r;
-        int y = r % 2 == 0 ? num - 1 - r * n : n + r * n - num;
-        return board[x][y];
-}
 }
